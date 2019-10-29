8a992f2e40ce679ae3f08ec9389142a24321bf23
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 15:59:19 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    all_actors = []
+    self.actors.each do |actor|
+      all_actors << actor.full_name
+    end
+    all_actors
+  end
+
 end

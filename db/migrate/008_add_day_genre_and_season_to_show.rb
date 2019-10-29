e757291e5c778500cfa3730b99b44cc47d1abd42
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 15:58:14 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_day_genre_and_season_to_show.rb
+++ db/migrate/008_add_day_genre_and_season_to_show.rb
@@ -1,3 +1,5 @@
 class AddDayGenreAndSeasonToShows < ActiveRecord::Migration[5.2]
-  add_column :shows
+  add_column :shows, :day, :string
+  add_column :shows, :genre, :string
+  add_column :shows, :season, :string
 end

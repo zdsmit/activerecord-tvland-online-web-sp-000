b0ad6fc456e6e80af908a24388191a940910d85d
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 15:33:02 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    all_roles = []
+    self.characters.each do |role|
+      all_roles << role.name
+    end
+    all_roles
+  end
+
 end

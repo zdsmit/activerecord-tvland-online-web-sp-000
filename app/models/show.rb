efccd00003b86864498e4492009b60006e924211
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 15:46:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,13 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    all_actors = []
+    self.actors.each do |actor|
+      all_actors << actor
+    end
+    all_actors
+  end
+
 end
